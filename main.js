5736d6797a5eb1a4bef0b713170cf51851e5fc60
Muhammad Shakir <chaudhryshakir67@gmail.com>
2023-09-07 15:34:17 +0500

    Main.js file ⚡

---
+++ main.js
@@ -0,0 +1,6 @@
+let navbarButton = document.querySelector('.navbar-toggler')
+
+navbarButton.addEventListener('click', () => {
+  navbarButton.style.border = '1px solid black'
+  navbarButton.style.outline = 'none'
+})
